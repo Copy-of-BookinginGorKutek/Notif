ceab39f91b76a40710ee1fb1894082b31e2796d4
Jaycent Gunawan Ongris <jaycent.gunawan@ui.ac.id>
2023-05-21 21:50:49 +0700

    finishing for homepage

--- src/main/java/com/b2/notification/service/NotificationServiceImpl.java
+++ src/main/java/com/b2/notification/service/NotificationServiceImpl.java
@@ -2,32 +2,24 @@ package com.b2.notification.service;
 
 import com.b2.notification.dto.NotificationRequest;
 import com.b2.notification.exceptions.NotificationNotFoundException;
-import com.b2.notification.exceptions.UserNotFoundException;
 import com.b2.notification.model.Notification;
 import com.b2.notification.model.StatusPembayaranNotif;
 import com.b2.notification.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
 import java.util.List;
 @Service
 public class NotificationServiceImpl implements NotificationService{
     private final NotificationRepository notificationRepository;
-    private final RestTemplate restTemplate;
-
     @Autowired
     public NotificationServiceImpl(NotificationRepository notificationRepository, RestTemplate restTemplate) {
         this.notificationRepository = notificationRepository;
-        this.restTemplate = restTemplate;
     }
     @Override
     public Notification createNotification(NotificationRequest request) {
-        if (Boolean.FALSE.equals(isUserExist(request.getEmailUser()))){
-            throw new UserNotFoundException(request.getEmailUser());
-        }
         String message = getStatusPembayaranNotifById(request.getStatusId()).getMessage();
         Notification notification = Notification.builder()
                 .emailUser(request.getEmailUser())
@@ -40,9 +32,6 @@ public class NotificationServiceImpl implements NotificationService{
 
     @Override
     public List<Notification> findByEmailUser(String emailUser) {
-        if (Boolean.FALSE.equals(isUserExist(emailUser))){
-            throw new UserNotFoundException(emailUser);
-        }
         return notificationRepository.findAllByEmailUser(emailUser);
     }
 
@@ -52,11 +41,6 @@ public class NotificationServiceImpl implements NotificationService{
             throw new NotificationNotFoundException(id);
         }
         notificationRepository.deleteById(id);
-    }
-
-    @Override
-    public Boolean isUserExist(String emailUser) {
-        return true;
     }
 
     public StatusPembayaranNotif getStatusPembayaranNotifById(Integer id){
