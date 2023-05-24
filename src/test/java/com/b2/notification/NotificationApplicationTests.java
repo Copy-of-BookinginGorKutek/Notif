c843961745f921fa32f48cc3f82ddec05992238e
Jaycent Gunawan Ongris <jaycent.gunawan@ui.ac.id>
2023-05-24 11:21:47 +0700

    fix context loads

--- src/test/java/com/b2/notification/NotificationApplicationTests.java
+++ src/test/java/com/b2/notification/NotificationApplicationTests.java
@@ -3,11 +3,14 @@ package com.b2.notification;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class NotificationApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
